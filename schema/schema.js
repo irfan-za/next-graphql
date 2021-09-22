72698b304fc5758cd78ffb128f388745b27698d9
irfan-za <fanzahran123@gmail.com>
2021-09-22 14:25:46 +0700

    testing query in graphql

--- schema/schema.js
+++ schema/schema.js
@@ -2,6 +2,13 @@ const graphql = require('graphql');
 
 const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql;
 
+// contoh simple database
+const book = [
+  {name: 'Name of the Wind', genre: 'Fantasy', id: '1'},
+  {name: 'The Final Empire', genre: 'Fantasy', id: '2'},
+  {name: 'The Long Earth', genre: 'Sci-Fi', id: '3'}
+]
+
 const BookType = new GraphQLObjectType({
   name    : "book",
   fields  : ()=>({
@@ -19,10 +26,12 @@ const RootQuery =new GraphQLObjectType({
       args: {id: {type : GraphQLString}},
       resolve(parents, args){
         // console get data from db
+        return book.find(book => book.id == args.id)
       }
     }
   }
-})
+});
+
 
 module.exports = new GraphQLSchema({
   query : RootQuery
