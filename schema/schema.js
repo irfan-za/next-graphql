b7adba655b2f0319a035a183900e42fb393e883f
irfan-za <fanzahran123@gmail.com>
2021-09-23 22:03:28 +0700

    adding graphqlList

--- schema/schema.js
+++ schema/schema.js
@@ -4,13 +4,17 @@ const {GraphQLObjectType,
       GraphQLString,
       GraphQLSchema,
       GraphQLID,
-      GraphQLInt} = graphql;
+      GraphQLInt,
+      GraphQLList} = graphql;
 
 // contoh simple database
 const books = [
-  {name: 'Name of the Wind', genre: 'Fantasy', id: '1'},
-  {name: 'The Final Empire', genre: 'Fantasy', id: '2'},
-  {name: 'The Long Earth', genre: 'Sci-Fi', id: '3'}
+  {name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1'},
+  {name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2'},
+  {name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3'},
+  {name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2'},
+  {name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3'},
+  {name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3'}
 ]
 const authors =  [
   {name: 'Patrick Rothfuss', age: 44, id:"1"},
@@ -23,7 +27,13 @@ const BookType = new GraphQLObjectType({
   fields  : ()=>({
     id : {type: GraphQLID},
     name : {type: GraphQLString},
-    genre : {type: GraphQLString}
+    genre : {type: GraphQLString},
+    author :{
+      type : AuthorType,
+      resolve(parent, args){
+        return authors.find(author => author.id == parent.authorId)
+      }
+    }
   })
 })
 const AuthorType = new GraphQLObjectType({
@@ -31,7 +41,13 @@ const AuthorType = new GraphQLObjectType({
   fields  : ()=>({
     id : {type: GraphQLID},
     name : {type: GraphQLString},
-    age : {type: GraphQLInt}
+    age : {type: GraphQLInt},
+    books : {
+      type : new GraphQLList(BookType),
+      resolve(parent, args){
+        return books.filter(book => book.authorId == parent.id)
+      }
+    }
   })
 })
 
