0ea78ed7c7bfc0de4e5a647d7ea0125d2a00cd01
irfan-za <fanzahran123@gmail.com>
2021-09-30 12:43:26 +0700

    fix cross-origin

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const {graphqlHTTP} =  require('express-graphql');
 const schema = require('./schema/schema')
+const mongoose = require('mongoose')
+const cors = require('cors')
 
 const app = express();
+
+// allow cross-origin-request
+app.use(cors())
+
+// connect to Database
+mongoose.connect("mongodb+srv://irfan:123@gql.xqmak.mongodb.net/gql?retryWrites=true&w=majority");
+mongoose.connection.once('open', ()=>{
+  console.log("connected to DB!")
+})
+
 
 app.use('/graphql', graphqlHTTP({
   schema,
   graphiql : true
 }));
-app.listen(3000, ()=>{
-  console.log("kokok 3000")
+app.listen(4000, ()=>{
+  console.log("Running on port 4000")
 });
